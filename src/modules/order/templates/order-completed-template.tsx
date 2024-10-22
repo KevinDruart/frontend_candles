2c3f804cfac15480d30312a398b19339de40d95a
Druart <65339848+KevinDruart@users.noreply.github.com>
2024-10-22 04:11:05 +0200

    Update order-completed-template.tsx

--- src/modules/order/templates/order-completed-template.tsx
+++ src/modules/order/templates/order-completed-template.tsx
@@ -28,12 +28,12 @@ export default function OrderCompletedTemplate({
             level="h1"
             className="flex flex-col gap-y-3 text-ui-fg-base text-3xl mb-4"
           >
-            <span>Thank you!</span>
-            <span>Your order was placed successfully.</span>
+            <span>Merci!</span>
+            <span>Votre commande a été enregistrée avec succès ! 🎊</span>
           </Heading>
           <OrderDetails order={order} />
           <Heading level="h2" className="flex flex-row text-3xl-regular">
-            Summary
+            Récapitulatif 
           </Heading>
           <Items items={order.items} region={order.region} />
           <CartTotals data={order} />
